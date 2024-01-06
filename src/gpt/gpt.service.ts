99fa91ada958f5025c2cec57be9b7029989979ad
alexdevrr05 <pv190115553@vallarta.tecmm.edu.mx>
2024-01-06 09:41:46 -0600

    Add OpenAI library and integrate with GptService

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -2,12 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
+import OpenAI from 'openai';
 
 @Injectable()
 // Solo va a llamar casos de usos
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -5,15 +5,24 @@
  * para trabajar
  */
 
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
 
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-3.5-turbo',
+  });
+
+  console.log(completion.choices[0]);
+
+  return completion.choices[0];
 };
